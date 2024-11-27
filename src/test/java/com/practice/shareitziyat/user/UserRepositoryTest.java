f9d9f66297761e870e58a28ce4f22585cc1c7103
DinaraZZ <dinara.rakhymbekova@gmail.com>
2024-11-27 15:51:46 +0500

    user service test added

--- src/test/java/com/practice/shareitziyat/user/UserRepositoryTest.java
+++ src/test/java/com/practice/shareitziyat/user/UserRepositoryTest.java
@@ -1,4 +1,90 @@
 package com.practice.shareitziyat.user;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@AutoConfigureTestDatabase
 public class UserRepositoryTest {
+    @Autowired
+    UserRepository userRepository;
+
+    @Test
+    public void findByIdTest() {
+        User user = new User();
+        user.setId(1L);
+        user.setName("User1");
+        user.setEmail("email1@email.com");
+        userRepository.save(user);
+
+        Optional<User> userOptional = userRepository.findById(1L);
+
+        assertTrue(userOptional.isPresent());
+        assertEquals(user.getId(), userOptional.get().getId());
+        assertEquals("User1", userOptional.get().getName());
+        assertEquals("email1@email.com", userOptional.get().getEmail());
+    }
+
+    @Test
+    public void findAllTest() {
+        User user1 = new User();
+        user1.setId(1L);
+        user1.setName("User1");
+        user1.setEmail("email1@email.com");
+        userRepository.save(user1);
+        User user2 = new User();
+        user2.setId(2L);
+        user2.setName("User2");
+        user2.setEmail("email2@email.com");
+        userRepository.save(user2);
+
+        List<User> users = userRepository.findAll();
+
+        assertEquals(2, users.size());
+        assertTrue(users.contains(user1));
+        assertTrue(users.contains(user2));
+        assertEquals( user1.getId(), users.get(0).getId());
+        assertEquals( user2.getId(), users.get(1).getId());
+        assertEquals( user1.getName(), users.get(0).getName());
+        assertEquals( user2.getName(), users.get(1).getName());
+        assertEquals(user1.getEmail(), users.get(0).getEmail());
+        assertEquals(user2.getEmail(), users.get(1).getEmail());
+    }
+
+    @Test
+    public void findByEmailTest() {
+        User user = new User();
+        user.setId(1L);
+        user.setName("User1");
+        user.setEmail("email1@email.com");
+        userRepository.save(user);
+
+        Optional<User> userOptional = userRepository.findByEmail("email1@email.com");
+
+        assertTrue(userOptional.isPresent());
+        assertEquals(user.getId(), userOptional.get().getId());
+        assertEquals("User1", userOptional.get().getName());
+        assertEquals("email1@email.com", userOptional.get().getEmail());
+    }
+
+    @Test
+    public void deleteByIdTest() {
+        User user = new User();
+        user.setId(1L);
+        user.setName("User1");
+        user.setEmail("email1@email.com");
+        userRepository.save(user);
+
+        userRepository.deleteById(1L);
+
+        List<User> users = userRepository.findAll();
+        assertEquals(0, users.size());
+    }
 }
--- src/test/java/com/practice/shareitziyat/user/UserServiceTest.java
+++ src/test/java/com/practice/shareitziyat/user/UserServiceTest.java
@@ -1,15 +1,16 @@
 package com.practice.shareitziyat.user;
 
 import com.practice.shareitziyat.exceptions.NotFoundException;
+import com.practice.shareitziyat.exceptions.UserAlreadyExistsException;
 import com.practice.shareitziyat.user.dto.UserMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -21,7 +22,6 @@ public class UserServiceTest {
 
     @Spy
     UserMapper userMapper;
-
 
     @Test
     void findByIdTest() {
@@ -51,6 +51,99 @@ public class UserServiceTest {
         assertEquals(user.getId(), byId.getId());
         assertEquals(user.getEmail(), byId.getEmail());
         assertEquals(user.getName(), byId.getName());
+    }
 
+    @Test
+    void createExceptionTest() {
+        UserService userService = new UserServiceImpl(userRepository, userMapper);
+        User user = new User();
+        user.setId(1L);
+        user.setEmail("user@mail.com");
+        user.setName("user");
+
+        Mockito.when(userRepository.findByEmail(Mockito.anyString()))
+                .thenAnswer(invocation -> {
+                    User user2 = new User();
+                    user2.setId(2L);
+                    user2.setEmail("user2@mail.com");
+                    user2.setName("user2");
+                    return Optional.of(user2);
+                });
+
+        UserAlreadyExistsException ex = assertThrows(UserAlreadyExistsException.class,
+                () -> userService.create(user));
+
+        assertEquals("Пользователь с email:" + user.getEmail() + "уже существует", ex.getMessage());
+    }
+
+    @Test
+    void createTest() {
+        UserService userService = new UserServiceImpl(userRepository, userMapper);
+        User user = new User();
+        user.setId(1L);
+        user.setEmail("user@mail.com");
+        user.setName("user");
+
+        Mockito.when(userRepository.findByEmail(Mockito.anyString()))
+                .thenReturn(Optional.of(user));
+        Mockito.when(userRepository.save(Mockito.any(User.class)))
+                .thenReturn(user);
+
+        User created = userService.create(user);
+        assertEquals(user.getId(), created.getId());
+        assertEquals(user.getName(), created.getName());
+        assertEquals(user.getEmail(), created.getEmail());
+    }
+
+    @Test
+    void updateTest() {
+        UserService userService = new UserServiceImpl(userRepository, userMapper);
+        User user = new User();
+        user.setEmail("user@mail.com");
+        user.setName("user");
+
+        Mockito.when(userRepository.findByEmail(Mockito.anyString()))
+                .thenReturn(Optional.of(user));
+        Mockito.when(userRepository.findById(Mockito.anyLong()))
+                .thenAnswer(invocationOnMock -> {
+                    long userId = invocationOnMock.getArgument(0);
+                    User user2 = new User();
+                    user2.setId(userId);
+                    user2.setEmail("user2@mail.com");
+                    user2.setName("user2");
+                    return Optional.of(user2);
+                });
+        Mockito.when(userRepository.save(Mockito.any(User.class)))
+                .thenAnswer(invocationOnMock -> invocationOnMock.getArgument(0));
+
+        User updated = userService.update(user, 1L);
+        assertEquals(user.getId(), updated.getId());
+        assertEquals(user.getName(), updated.getName());
+        assertEquals(user.getEmail(), updated.getEmail());
+    }
+
+    @Test
+    void findAllTest() {
+        UserService userService = new UserServiceImpl(userRepository, userMapper);
+        User user1 = new User();
+        user1.setId(1L);
+        user1.setName("User1");
+        user1.setEmail("email1@email.com");
+        User user2 = new User();
+        user2.setId(2L);
+        user2.setName("User2");
+        user2.setEmail("email2@email.com");
+
+        Mockito.when(userRepository.findAll())
+                .thenReturn(List.of(user1, user2));
+
+        List<User> users = userService.findAll();
+        assertEquals(2, users.size());
+        assertEquals(user1.getId(), users.get(0).getId());
+        assertEquals(user2.getId(), users.get(1).getId());
+        assertEquals(user1.getName(), users.get(0).getName());
+        assertEquals(user2.getName(), users.get(1).getName());
+        assertEquals(user1.getEmail(), users.get(0).getEmail());
+        assertEquals(user2.getEmail(), users.get(1).getEmail());
     }
 }
