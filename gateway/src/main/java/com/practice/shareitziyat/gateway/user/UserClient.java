b0845cb31b93e1c75c1188296289a5ab96b55b9b
DinaraZZ <dinara.rakhymbekova@gmail.com>
2025-03-02 19:00:41 +0500

    docker 2

--- gateway/src/main/java/com/practice/shareitziyat/gateway/user/UserClient.java
+++ gateway/src/main/java/com/practice/shareitziyat/gateway/user/UserClient.java
@@ -9,6 +9,7 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.DefaultUriBuilderFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 
 import java.util.Map;
 
@@ -21,7 +22,7 @@ public class UserClient extends BaseClient {
         super(
                 builder
                         .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl + API_PREFIX))
-                        .requestFactory(HttpComponentsClientHttpRequestFactory::new)
+                        .requestFactory(HttpComponentsClientHttpRequestFactory.class)
                         .build()
         );
     }
